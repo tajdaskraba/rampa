2812e161aade25533d24680e98acef51d24bfc79
tajdaskraba <47979019+tajdaskraba@users.noreply.github.com>
2025-06-06 18:24:02 +0200

    Update Leds.ino

--- Leds.ino
+++ Leds.ino
@@ -1,5 +1,5 @@
 #include <FastLED.h>
-#define NUM_LEDS 144
+#define NUM_LEDS 1
 #define DATA_PIN 6
 #define BRIGHTNESS 5
 
@@ -11,7 +11,7 @@ CRGB leds[NUM_LEDS];
 
     void loop() {
         for(int dot = 0; dot < NUM_LEDS; dot++) { 
-            leds[dot] = CRGB::Blue;
+            leds[dot] = CRGB::White;
             FastLED.show();
             leds[dot] = CRGB::Black;
             delay(30);
