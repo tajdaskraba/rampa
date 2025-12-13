b7fd33e1524343cfa49ece8a13f527537d45e49d
tajdaskraba <tajda.skraba@gmail.com>
2025-12-13 20:13:48 +0100

    dodaj godot projekt

---
+++ 5hek/godot/Player/scripts/state_walk.gd
@@ -0,0 +1,33 @@
+class_name State_Walk extends State
+
+@export var move_speed : float = 100.0
+@onready var idle: State = $"../Idle"
+@onready var walk: State = $"../Walk"
+
+# What happens when the player enters this state? 
+func Enter() -> void:
+	player.UpdateAnimation("walk")
+	pass 
+
+# What happens when the player exits this state?
+func Exit() -> void:
+	pass
+
+# What happens during the _process pdate in this State?
+func Process( _delta : float ) -> State:
+	if player.direction == Vector2.ZERO:
+		return idle
+		
+	player.velocity = player.direction * move_speed
+	
+	if player.SetDirection():
+		player.UpdateAnimation("walk")
+	return null
+
+# What happens during the _physics_process update in this State?
+func Physics( _delta : float ) -> State:
+	return null
+
+# What happens with input events in this State?
+func HandleInput( _event: InputEvent ) -> State:
+	return null
