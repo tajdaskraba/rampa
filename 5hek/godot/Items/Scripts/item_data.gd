cd19a6e2fd18c7ba5c64936a9b1a00f8241568da
tajdaskraba <tajda.skraba@gmail.com>
2026-04-22 13:52:57 +0200

    created placeholder branch

--- 5hek/godot/Items/Scripts/item_data.gd
+++
@@ -1,5 +0,0 @@
-class_name ItemData extends Resource
-
-@export var name : String = ""
-@export_multiline var description : String = ""
-@export var texture : Texture2D
--- 5hek/godot/Levels/Scripts/level.gd
+++
@@ -1,5 +0,0 @@
-extends Node2D
-
-func _ready() -> void:
-	self.y_sort_enabled = true
-	PlayerManager.set_as_parent( self )
